ca6fb71629fd426f63f9dedee445f482bfe923cd
Vagelis Stefanopoulos <vstefanopoulos@yahoo.gr>
2024-09-22 22:07:55 +0300

    improved playback with ticker

--- rhythmgenerator/play.go
+++ rhythmgenerator/play.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sync"
 	"time"
 
 	"github.com/gopxl/beep"
@@ -12,52 +11,35 @@ import (
 	"github.com/gopxl/beep/wav"
 )
 
-// const on = "./wav/rim.wav"
-// const filler = "./wav/side.wav"
-// const off = "./wav/hh.wav"
-
 var stopPlayChan = make(chan struct{})
-var stopPlayPatternChan = make(chan struct{})
 
 func play(pattern string, bpm int, w *widgets) {
 	if pattern == "" {
 		return
 	}
-
+	on := makeBuffer("./wav/rim.wav")
+	filler := makeBuffer("./wav/side.wav")
+	off := makeBuffer("./wav/hh.wav")
+	ticker := time.NewTicker(time.Duration(60000/bpm) * time.Millisecond)
 	var barCount int
-	var wg sync.WaitGroup
+
 	for {
-		select {
-		case <-stopPlayChan:
-			return
-		default:
-			barCount++
-
-			wg.Add(2)
-
-			go func() {
-				defer wg.Done()
-				w.bar.SetText(fmt.Sprintf("Bar: %v", barCount))
-			}()
-
-			go func() {
-				defer wg.Done()
-				playPattern(pattern, bpm, w.playFillsCheckbox.Checked, w.playOffsetsCheckbox.Checked)
-			}()
-
-			wg.Wait()
+		for _, char := range pattern {
+			select {
+			case <-stopPlayChan:
+				ticker.Stop()
+				return
+			case <-ticker.C:
+				playPattern(char, w, on, filler, off)
+			}
 		}
+		barCount++
+		w.bar.SetText(fmt.Sprint(barCount))
 	}
-
 }
 
 func stop() {
-	go func() {
-		stopPlayChan <- struct{}{}
-	}()
-	go func() {
-		stopPlayPatternChan <- struct{}{}
-	}()
+	stopPlayChan <- struct{}{}
 }
 
 func makeBuffer(file string) *beep.Buffer {
@@ -79,48 +61,30 @@ func makeBuffer(file string) *beep.Buffer {
 	return buffer
 }
 
-func playPattern(pattern string, bpm int, playFills, playOffset bool) {
-	durationPerBeat := time.Duration(60000/bpm) * time.Millisecond
-
-	on := makeBuffer("./wav/rim.wav")
-	filler := makeBuffer("./wav/side.wav")
-	off := makeBuffer("./wav/hh.wav")
-
-	for _, char := range pattern {
-		select {
-		case <-stopPlayPatternChan:
-			return
-		default:
-			switch char {
-			case 'X':
-				go func() {
-					snr := on.Streamer(0, on.Len())
-					speaker.Play(snr)
-				}()
-			case 'x':
-				if playFills {
-					go func() {
-						side := filler.Streamer(0, filler.Len())
-						speaker.Play(side)
-					}()
-				} else {
-					time.Sleep(durationPerBeat)
-					continue
-				}
-
-			case 'o':
-				if playOffset {
-					go func() {
-						hh := off.Streamer(0, off.Len())
-						speaker.Play(hh)
-					}()
-				} else {
-					time.Sleep(durationPerBeat)
-					continue
-				}
-
+func playPattern(char rune, w *widgets, on, filler, off *beep.Buffer) {
+	switch {
+	case char == 'X':
+		go func() {
+			snr := on.Streamer(0, on.Len())
+			speaker.Play(snr)
+		}()
+	case char == 'x':
+		go func() {
+			if w.playFillsCheckbox.Checked {
+				side := filler.Streamer(0, filler.Len())
+				speaker.Play(side)
+			} else {
+				return
 			}
-			time.Sleep(durationPerBeat)
-		}
+		}()
+	case char == 'o':
+		go func() {
+			if w.playOffsetsCheckbox.Checked {
+				hh := off.Streamer(0, off.Len())
+				speaker.Play(hh)
+			} else {
+				return
+			}
+		}()
 	}
 }
