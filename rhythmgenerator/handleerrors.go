30def8b627263908fb0ad0dcc49c308b14444838
Vagelis Stefanopoulos <vstefanopoulos@yahoo.gr>
2024-09-28 12:17:25 +0300

    fixed minor bug

--- rhythmgenerator/handleerrors.go
+++ rhythmgenerator/handleerrors.go
@@ -3,6 +3,7 @@ package rhythmgenerator
 func handleErrors(w *Widgets, p *Parameters, prev *PreviousState) *Error {
 	var e *Error
 	e = convertInput(w, p)
+	updatePrev(w, prev)
 	if e != nil {
 		initialButtonState(w)
 		w.inversionLabel.SetText(e.Message)
@@ -14,7 +15,6 @@ func handleErrors(w *Widgets, p *Parameters, prev *PreviousState) *Error {
 		}
 		return e
 	}
-	updatePrev(w, prev)
 	w.updateInversionLabel(p.inversionDegree)
 	w.updatePatternLabel(*p.pattern)
 	return nil
